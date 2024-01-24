2fb2c3542844ea573d3e8cc97ed6c7a7868301c3
Fatulla <fatulla2004@gmail.com>
2024-01-24 12:21:00 +0400

    added templates

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template, request
 import psycopg2
 import config as cfg
+from psycopg2 import sql
+
+
 app = Flask(__name__)
+
+db_params = cfg.db_params
+
+@app.route('/')
+def home():
+    return render_template('home.html')
 
 @app.route('/search/<int:book_id>', methods=['GET'])
 def search_book(book_id):
@@ -30,6 +39,60 @@ def search_book(book_id):
                         'Publish_year': book_data[3], 'Price': book_data[4]})
     else:
         return jsonify({'message': 'Book not found'}), 404
+    
+@app.route('/add_user_form')
+def add_user_form():
+    return render_template('add_user_form.html')
+
+# -----------------  POST ----------------
+def execute_query(query, params=None):
+    connection = None
+    try:
+        # Connect to the PostgreSQL database
+        connection = psycopg2.connect(**db_params)
+        cursor = connection.cursor()
+
+        # Execute the query
+        if params:
+            cursor.execute(query, params)
+        else:
+            cursor.execute(query)
+
+        # Commit the changes
+        connection.commit()
+
+    except psycopg2.Error as e:
+        # Handle database error
+        if connection:
+            connection.rollback()
+        return str(e), 500
+
+    finally:
+        # Close the database connection
+        if connection:
+            connection.close()
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    # Get form data from the request
+    username = request.form.get('username')
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    # Check if the email is already registered
+    query_check_email = sql.SQL("SELECT 1 FROM users WHERE email = {} LIMIT 1").format(sql.Literal(email))
+    result = execute_query(query_check_email)
+    if result and result[0]:
+        return jsonify({'message': 'Email already registered'}), 400
+
+    # Insert the new user into the database
+    query_insert_user = sql.SQL("INSERT INTO users (username, email, password) VALUES ({}, {}, {})").format(
+        sql.Literal(username), sql.Literal(email), sql.Literal(password)
+    )
+
+    result = execute_query(query_insert_user)
+
+    return render_template('success.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
